6c332f8189d74a2c483b06bf7c8aeeab482adcb2
Xx_Victor_ESTRAN_xX <vic.estran@gmail.com>
2026-02-17 21:17:49 +0100

    Mise en place des fichiers de footer, header et nav avec architecture MVC respecté (layout.php) dans le dossié common, modification de index.php avec ajout de vrai contenus, premeir texte avec l'utilisation du templace css tailwindcss, implémentation basique avec choix de coulleurs base sur le thèmes de Stakes mais qui rend bizard une foit mit en place

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-	echo "grdtgdfgdfsoufysioufysdiufsdgifsdgifhjdsgfisdfjhsdgfjksdfgjsdhfgsdjfgsdjhfgds\n hdsofhfsdfdsf";
-echo "Hello World!";
-?>
-fezoigfezkugfezkufdgezoiudegieufgeeeee
-<h1>caca prout</h1>
+$content = "<h2>Bienvenue sur Gambling.io</h2>";
 
-eeeeeeeeee
-<h1>caca pdezfdtufyihypuyuyftdsghtfguyrout</h1>
-
-eeeeeeeeee
-<h1>caca pdezfdtufyihypuyuyftdsghtfguyrout</h1>
+include "view/layout.php";
