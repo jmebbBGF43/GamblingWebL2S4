77ed771a7c1f387b97d66f9f0a106857f35a4d7b
Xx_Victor_ESTRAN_xX <vic.estran@gmail.com>
2026-02-19 00:04:07 +0100

    Fixe : rectification de l'arondie de l'angle de la nav

--- view/common/_nav.php
+++ view/common/_nav.php
@@ -1,5 +1,5 @@
 <aside class="w-64 flex flex-col pl-4 py-4">
-    <nav class="flex-1 flex-col px-10 bg-[#1a2d39] rounded-xl border border-white/5">
+    <nav class="flex-1 flex-col px-10 bg-[#1a2d39] rounded border border-white/5">
 
     </nav>
 </aside>
