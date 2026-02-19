b868670b4827abae5dec85a9e4be637efbf01a5a
Quincieu Thomas <quincieu.thomas@gmail.com>
2026-02-19 13:51:10 +0100

    v2 de login

--- view/pages/login.php
+++ view/pages/login.php
@@ -1,3 +1,19 @@
-<div class="w-64 mx-auto flex bg-[#0f212d] rounded-xl -p6">
-    <p>test</p>
+<div class="h-full w-full flex items-center justify-center">
+
+    <div class="w-64 flex flex-col gap-6 bg-[#0f212d] rounded-xl p-6">
+        <div>
+            <p class="text-white">Se connecter </p>
+        </div>
+        <div>
+            <input type="text" placeholder="Identifiant" class="w-full p-2 rounded bg-white text-black"/>
+        </div>
+        <div>
+            <input type="password" placeholder="Mot de passe" class="w-full p-2 rounded bg-white text-black""/>
+        </div>
+        <hr>
+        <a href="#" class="btn btn-primary rounded-xl font-bold border-none bg-[#1576e2] text-white">
+            Connection
+        </a>
+    </div>
+
 </div>
