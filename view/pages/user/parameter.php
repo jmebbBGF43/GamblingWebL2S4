b52b7e21f06ca35085006b2ab0544f6c7e11e60d
Xx_Victor_ESTRAN_xX <vic.estran@gmail.com>
2026-02-26 02:15:12 +0100

    - Modifications de la page de Payment avec ajout de crédits et pareille pour paramater

--- view/pages/user/parameter.php
+++ view/pages/user/parameter.php
@@ -1,3 +1,6 @@
-<div class="flex w-full text-bold text-3xl font-bold h-20 border-b border-white/5 pt-3">
-    Parametre :
+<div class="max-w-4xl mx-auto p-6 bg-[#1a2c38] rounded border border-white/10 shadow-2xl text-[#9d9d9d]">
+    <h2 class="text-3xl font-bold text-white mb-8 text-center">Paramètres : </h2>
+    <p>
+        [liste des paramètres pas sûre quel reste par ce qu'on ne sait pas quoi trop mettre comme paramètres ]
+    </p>
 </div>
