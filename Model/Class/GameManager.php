02e4388e22743e2e946cef3924ef3a0a948d45e5
Xx_Victor_ESTRAN_xX <vic.estran@gmail.com>
2026-03-27 00:23:15 +0100

    - Modifications des return des focntions dasn GameManager.php

--- Model/Class/GameManager.php
+++ Model/Class/GameManager.php
@@ -15,51 +15,43 @@ class GameManager
     public function getIDs(): array
     {
         $sql = "SELECT id FROM games;";
-        $stmt = $this->db->query($sql);
-        return $stmt->fetchAll();
+        return $this->db->query($sql)->fetchAll();
     }
     public function getSlugs(): array
     {
         $sql = "SELECT slug FROM games;";
-        $stmt = $this->db->query($sql);
-        return $stmt->fetchAll();
+        return $this->db->query($sql)->fetchAll();
     }
 
     public function getSpecificID(string $slug): array
     {
         $sql = "SELECT id FROM games WHERE slug = '$slug';";
-        $stmt = $this->db->query($sql);
-        return $stmt->fetchAll();
+        return $this->db->query($sql)->fetchAll();
     }
 
     public function getSpecificSlug(int $id): array
     {
         $sql = "SELECT id FROM games WHERE slug = '$id';";
-        $stmt = $this->db->query($sql);
-        return $stmt->fetchAll();
+        return $this->db->query($sql)->fetchAll();
     }
     public function getNameFromID(int $int): array
     {
         $sql = "SELECT name FROM games WHERE id = '$int';";
-        $stmt = $this->db->query($sql);
-        return $stmt->fetchAll();
+        return $this->db->query($sql)->fetchAll();
     }
     public function getNameFromSlug(string $slug): array
     {
         $sql = "SELECT name FROM games WHERE slug = '$slug';";
-        $stmt = $this->db->query($sql);
-        return $stmt->fetchAll();
+        return $this->db->query($sql)->fetchAll();
     }
     public function getActive(int $id): array
     {
         $sql = "SELECT is_active FROM games WHERE id = '$id';";
-        $stmt = $this->db->query($sql);
-        return $stmt->fetchAll();
+        return $this->db->query($sql)->fetchAll();
     }
     public function getProbs(int $id): array
     {
         $sql = "SELECT probabilities FROM games WHERE id = '$id';";
-        $stmt = $this->db->query($sql);
-        return $stmt->fetchAll();
+        return $this->db->query($sql)->fetchAll();
     }
 }
