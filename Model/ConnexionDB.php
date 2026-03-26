465d74cd0bc397779864614e40d81310656f8126
jmebbBGF43 <vic.estran@gmail.com>
2026-03-26 12:32:55 +0100

    modifications de la connexion à la db pour que ça marche et que ça respecte plus les principes

--- Model/ConnexionDB.php
+++ Model/ConnexionDB.php
@@ -5,20 +5,26 @@ use Model\ConfigurationDB;
 use \PDO;
 use \PDOException;
 class ConnexionDB {
-    private static PDO $pdo;
+    private static ?PDO $pdo = null;
+
     private function __construct() {
         try {
             $config = new ConfigurationDB();
-            $this->pdo = new PDO($config->getDNS(), $config->getUsername(), $config->getPassword(), array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC));
+            self::$pdo = new PDO(
+                $config->getDNS(),
+                $config->getUsername(),
+                $config->getPassword(),
+                [PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
+            );
         } catch (PDOException $e) {
             die("Erreur de connexion à la DB : " . $e->getMessage());
         }
     }
 
-    public function getPDO(): PDO
+    public static function getPDO(): PDO
     {
-        if (!self::$pdo) {
-            $this->__construct();
+        if (self::$pdo === null) {
+            new self();
         }
         return self::$pdo;
     }
